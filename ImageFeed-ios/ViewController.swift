f1434558f228832a21dd697fa68f4177b6453c92
iamjohansson <ma4ujloba@gmail.com>
2023-07-31 20:08:06 +0300

    Initial Commit

---
+++ ImageFeed-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed-ios
+//
+//  Created by Admin on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
