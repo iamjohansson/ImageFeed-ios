3ebf0e727eac5388df51ba686854576dc6a9ab87
iamjohansson <ma4ujloba@gmail.com>
2023-08-14 20:58:17 +0300

    Добавлен таб бар контроллер для переключения между вьюхами.

--- ImageFeed-ios/Profile/ProfileViewController.swift
+++ ImageFeed-ios/Profile/ProfileViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
