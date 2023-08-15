45ca584c2f3d62f8ec7021acabbf52783749c775
iamjohansson <ma4ujloba@gmail.com>
2023-08-15 21:35:31 +0300

    Реализован сигвей, при переходе с таблицы запоминаем картинку и передаем ее на необходимую нам вью

--- ImageFeed-ios/ImageList/ImageListViewController.swift
+++ ImageFeed-ios/ImageList/ImageListViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
-class ImageListViewController: UIViewController {
+final class ImageListViewController: UIViewController {
     
     @IBOutlet private var tableView: UITableView!
     
     private let photos: [String] = Array(0...20).map{ "\($0)" }
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         .lightContent
@@ -13,6 +14,17 @@ class ImageListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            let viewController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            let image = UIImage(named: photos[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
     
     private func configCell(for cell: ImageListCell, with indexPath: IndexPath) {
@@ -49,6 +61,7 @@ extension ImageListViewController: UITableViewDataSource {
 extension ImageListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
--- ImageFeed-ios/Profile/ProfileViewController.swift
+++ ImageFeed-ios/Profile/ProfileViewController.swift
@@ -2,12 +2,13 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
 
+    @IBOutlet private weak var avatarImage: UIImageView!
+    @IBOutlet private weak var logoutButton: UIButton!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var loginLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         .lightContent
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
     }
 }
