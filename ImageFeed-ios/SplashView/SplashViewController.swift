30aef182e8ad2b51ce528e38a67d550c76bde638
iamjohansson <ma4ujloba@gmail.com>
2023-09-12 21:49:01 +0300

    Добавлена зависимость HUD прогресс бара через SPM

--- ImageFeed-ios/SplashView/SplashViewController.swift
+++ ImageFeed-ios/SplashView/SplashViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ProgressHUD
 
 final class SplashViewController: UIViewController {
     private let ShowAuthenticationScreenSegueIdentifier = "ShowAuthenticationScreen"
@@ -48,6 +49,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        ProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -60,7 +62,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
+                ProgressHUD.dismiss()
             case .failure:
+                ProgressHUD.dismiss()
                 // TODO [Sprint 11]
                 break
             }
