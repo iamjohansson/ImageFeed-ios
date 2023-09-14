2b39b58eb8785817233b936be7bebf6d91b016e4
iamjohansson <ma4ujloba@gmail.com>
2023-09-14 22:25:25 +0300

    Устранение состояния гонки

--- ImageFeed-ios/SplashView/SplashViewController.swift
+++ ImageFeed-ios/SplashView/SplashViewController.swift
@@ -49,7 +49,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.show()
+        UIBlockingProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -62,12 +62,11 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
-                ProgressHUD.dismiss()
             case .failure:
-                ProgressHUD.dismiss()
                 // TODO [Sprint 11]
                 break
             }
+            UIBlockingProgressHUD.dismiss()
         }
     }
 }
